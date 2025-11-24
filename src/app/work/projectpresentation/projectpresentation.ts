2f77001a444369708404f0a36207520489164920
Pauline Wargny <wargny.pauline@gmail.com>
2025-11-24 02:53:40 +0100

    update: work section in progress (3/?)

--- src/app/work/projectpresentation/projectpresentation.ts
+++ src/app/work/projectpresentation/projectpresentation.ts
@@ -1,103 +1,37 @@
 import {Component, input} from '@angular/core';
 import {ProjectInfo} from '../projectinfo';
 import {RouterLink} from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialog } from '../../confirm-dialog/confirm-dialog';
 
 @Component({
   selector: 'app-projectpresentation',
-  imports: [RouterLink],
   template: `
-    <div class="item" data-bs-toggle="modal" data-bs-target="#popup-notitleyet" data-aos="zoom-in" data-aos-once="true">
+
+    <div data-aos="zoom-in" data-aos-once="true" (click)="openConfirmDialog()">
       <figure class="projectimage" style="background-image:url('{{ projectPresentation().photos[0] }}')">
         <figcaption>{{ projectPresentation().name }}</figcaption>
       </figure>
     </div>
-
-    <div class="modal fade">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="modal-button" data-bs-dismiss="modal"></button>
-          </div>
-          <div class="modal-body">
-            <h1>{{ projectPresentation().name }}</h1>
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-6">
-                  <div class="description">
-                    <h3>Présentation</h3>
-                    @for(projectPresentationLine of projectPresentation().presentation; track $index) {
-                      <p>{{ projectPresentationLine }}</p>
-                    }
-                    <br>
-
-                    @if(projectPresentation().contribution){
-                      <h3>Contribution personnelle</h3>
-                      <ul>
-                      @for(projectPresentationContribution of projectPresentation().contribution; track $index) {
-                        <li>{{ projectPresentationContribution }}</li>
-                      }
-                      </ul>
-                    }
-                  
-                    <div class="liste-langages">
-
-                      @for(projectPresentationLanguage of projectPresentation().languages; track $index) {
-                        <div class="langage-encadre">{{ projectPresentationLanguage }}</div>
-                      }
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-lg-6">
-                    <div class="carousel-inner">
-
-                      @for(projectPresentationPhoto of projectPresentation().photos; track $index) {
-                        <div class="carousel-item active">
-                        <img src="{{ projectPresentationPhoto }}" class="d-block w-100" alt="...">
-                        </div>
-                      }
-                    </div>
-
-                  <div id="carousel-notitle" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel-notitle"
-                      data-bs-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carousel-notitle"
-                      data-bs-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Next</span>
-                    </button>
-
-
-                    <div class="carousel-indicators">
-
-                      @for(projectPresentationPhoto of projectPresentation().photos; track $index) {
-                        <button type="button" data-bs-target="#carousel-notitle" class="active"
-                        aria-current="true" aria-label="Slide {{ $index }}"></button>
-                      }
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              @if(projectPresentation().code){
-                <button class="raise"><a href="{{projectPresentation().code}}" target="_blank">Code</a></button>
-              }
-
-              @if(projectPresentation().exe){
-                <button class="raise"><a href="{{projectPresentation().exe}}" target="_blank">Download</a></button>
-              }
-              
-            </div>
-          </div>
-        </div>
-      </div>
   `,
   styleUrls: ['./projectpresentation.css'],
 })
 export class ProjectPresentation {
   projectPresentation = input.required<ProjectInfo>();
+  constructor(private dialog: MatDialog) {
+
+    }
+  openConfirmDialog(): void {
+    const dialogRef = this.dialog.open(ConfirmDialog, {
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('User confirmed');
+      } else {
+        console.log('User cancelled');
+      }
+    });
+  }
 }
   
--- src/app/work/work.ts
+++ src/app/work/work.ts
@@ -10,12 +10,7 @@ import { ConfirmDialog } from '../confirm-dialog/confirm-dialog';
   selector: 'app-work',
   imports: [ProjectPresentation],
   template: `
-  <div style="text-align:center; margin-top: 100px;">
-  <h1>Angular Material Dialog Example</h1>
-  <button mat-raised-button color="primary" (click)="openConfirmDialog()">
-    Open Confirm Dialog
-  </button>
-</div>
+  
   <div id="gallery">
     
       @for(projects of projectList; track $index) {
@@ -33,19 +28,4 @@ export class Work {
   constructor(private dialog: MatDialog) {
     this.projectList = this.projects.getAllProjects();
   }
-  openConfirmDialog(): void {
-    const dialogRef = this.dialog.open(ConfirmDialog, {
-      width: '350px',
-      data: { message: 'Are you sure you want to proceed?' }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        console.log('User confirmed');
-      } else {
-        console.log('User cancelled');
-      }
-    });
-  }
-
 }
