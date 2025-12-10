2865350ce8cd8a2d761d9e5aed925bb217708c0a
Pauline Wargny <wargny.pauline@gmail.com>
2025-12-10 19:42:12 +0100

    update: burger menu for mobile

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,54 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet, 
+    RouterOutlet,
     RouterLink,
-    NgbModule
+    NgbModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatExpansionModule,
+    MatMenuModule
   ],
   templateUrl: "app.html",
   styleUrls: ['./app.css'],
 })
 export class App {
   title = 'homes';
+  menuOpen = false;
+
+  private lockScroll() {
+    document.body.classList.add('no-scroll');
+  }
+
+  private unlockScroll() {
+    document.body.classList.remove('no-scroll');
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  toggle() {
+    this.menuOpen = !this.menuOpen;
+
+    if (this.menuOpen) {
+      this.lockScroll();
+    } else {
+      this.unlockScroll()
+    }
+  }
+
+  close() {
+    this.menuOpen = false;
+    this.unlockScroll()
+  }
 }
