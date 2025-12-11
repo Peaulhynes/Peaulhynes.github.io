4dc21ef4a57e98dd9a191e8b5ec45433ffacc50c
Pauline Wargny <wargny.pauline@gmail.com>
2025-12-11 19:45:06 +0100

    Auto-generated commit

---
+++ chunk-N4PXECV5.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-ALRQMYFF.js";import"./chunk-KWXU5HX7.js";export{a as Skills};
---
+++ chunk-R3GSIU2D.js
@@ -0,0 +1,1 @@
+import{e as a}from"./chunk-ITKZ6B37.js";import"./chunk-KWXU5HX7.js";export{a as Work};
